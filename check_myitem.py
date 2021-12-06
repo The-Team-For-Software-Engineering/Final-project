f9ef3ff3ccd1459b1017215d1e0c5c002ac343fd
each_cheng <57981760+13459877099@users.noreply.github.com>
2021-12-06 19:32:04 +0800

    查看我的项目

---
+++ check_myitem.py
@@ -0,0 +1,48 @@
+#疑问：如何输出多组邀请码
+# coding=utf8
+import sys
+import time
+
+defaultencoding = 'utf-8'
+
+from flask import Flask, render_template, request, json,Blueprint
+import pymysql.cursors
+import json
+
+#导入模块
+Check_invite = Blueprint("check_invite", __name__)
+@Check_invite.route('/admin/item/check_myitem',methods=['POST'])
+def check_invite():
+    user_ID = str(json.loads(request.values.get("user_ID")))
+    conn = pymysql.Connect(
+        host='localhost',
+        port=3306,
+        user='woider',
+        passwd='3243',
+        db='python',
+        charset='utf8'
+    )#连接数据库
+    # 获取游标
+    cursor = conn.cursor()
+    #寻找根据user_ID确认user_admin=1所对应的project_key，再在相应的project_key找到对应的project_name和invitecode
+    sql = "select project_key, project_name, people_num, people_max, time_update, notes from project where project_key = any (select project_key from people_join where user_admin=1 and user_ID = '%s');" % (user_ID)
+    cursor.execute(sql)     
+    list = cursor.fetchall()
+
+    if len(list)>0:
+        s = 1
+        res = {}
+        res['code'] = s
+        res['list'] = list
+    else:
+        s = 0
+        res = {}
+        res['code'] = s
+        msg = "没有自己管理的项目"
+        res['msg'] = msg
+
+    cursor.close()
+    conn.close()
+    #关闭连接（方便其他接口之后的开启）
+
+    return json.dumps(res, ensure_ascii=False)

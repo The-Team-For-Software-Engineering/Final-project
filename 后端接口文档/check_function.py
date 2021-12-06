af4f02b0e1827910a6db1be0b711e2137e5d1689
each_cheng <57981760+13459877099@users.noreply.github.com>
2021-12-06 19:31:21 +0800

    查看功能

---
+++ 后端接口文档/check_function.py
@@ -0,0 +1,48 @@
+#疑问：如何输出多组邀请码
+# coding=utf8
+import sys
+import time
+
+defaultencoding = 'utf-8'
+
+from flask import Flask, render_template, request, json,Blueprint
+import pymysql.cursors
+import json
+
+#导入模块
+Check_function = Blueprint("check_invite", __name__)
+@Check_function.route('/admin/function/check_function',methods=['POST'])
+def check_function():
+    user_ID = str(json.loads(request.values.get("user_ID")))
+    conn = pymysql.Connect(
+        host='localhost',
+        port=3306,
+        user='woider',
+        passwd='3243',
+        db='python',
+        charset='utf8'
+    )#连接数据库
+    # 获取游标
+    cursor = conn.cursor()
+    
+    sql = "select project_key,project_type,project_name,time_create,time_update,user_ID,notes,download_num from functions"
+    cursor.execute(sql)     
+    list = cursor.fetchall()
+
+    if len(list)>0:
+        s = 1
+        res = {}
+        res['code'] = s
+        res['list'] = list
+    else:
+        s = 0
+        res = {}
+        res['code'] = s
+        msg = "没有内容"
+        res['msg'] = msg
+
+    cursor.close()
+    conn.close()
+    #关闭连接（方便其他接口之后的开启）
+
+    return json.dumps(res, ensure_ascii=False)
